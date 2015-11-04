fc40b475e308001dfba44f225a6b82912a921be0
Fabio SCHUBERT-GELBCKE <fschuber@dump-mini-2.42.fr>
2015-11-04 22:54:38 +0100

    finally 9 done

--- ex09/ssap2.php
+++ ex09/ssap2.php
@@ -1,17 +1,16 @@
 #!/usr/bin/php
 <?php
-        var_dump($argv);
         $chaine = "";
         foreach ($argv as $elem)
         {
-                echo $elem."\n\n";
                 if ($elem != $argv[0])
                 $chaine .= " $elem ";
         }
         $arr = preg_split('/[\s]+/', trim($chaine));
-        sort($arr);
+        natcasesort($arr);
         foreach($arr as $word)
         {if (ctype_alpha($word[0]))echo $word."\n";}        
+		sort($arr, SORT_STRING);
         foreach($arr as $word)
         {if (ctype_digit($word[0]))echo $word."\n";}
         foreach($arr as $word)
